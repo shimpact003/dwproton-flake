cfe7305dd993bba35394cc1c37d53b06ed6299fb
shimpact003 <42101670+shimpact003@users.noreply.github.com>
2026-08-25 00:58:09 +0000

    Update dwproton to 11.0-12

--- default.nix
+++ default.nix
@@ -5,11 +5,11 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "dwproton";
-  version = "10.0-26";
+  version = "11.0-12";
 
   src = fetchzip {
     url = "https://dawn.wine/dawn-winery/dwproton/releases/download/dwproton-${finalAttrs.version}/dwproton-${finalAttrs.version}-x86_64.tar.xz";
-    hash = "sha256-TkwhJCHPS0PdDIEL5GrxJPR09uO9U2DR8l9KWFLIF2g=";
+    hash = "sha256-NGyrXQcA+k87SnowFd41uq49luI32fZENTwFTma7NpI=";
   };
 
   dontUnpack = true;
